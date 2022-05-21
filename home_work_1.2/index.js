c1ef336538a0902e3d01319ccd29f6ab96873304
Olya_Koshelnikova <olha.koshelnikova@gmail.com>
2022-05-21 16:38:26 +0300

    add my last home work

---
+++ home_work_1.2/index.js
@@ -0,0 +1,8 @@
+
+let x  = +prompt("Enter the first number,please!");
+let y = +prompt("Enter the second number,please!");
+alert(x+y)
+
+// let x  = prompt("Enter the first number,please!");
+// let y = prompt("Enter the second number,please!");
+// alert(Number(x)+Number(y));
